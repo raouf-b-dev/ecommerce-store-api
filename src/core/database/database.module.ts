6ee02a49097ffcc8a7b397ca0fd86fa2a582cf1b
PRIMERaouf <rbdzmain@gmail.com>
2025-07-28 22:52:39 +0100

    create a database module and import TypeOrmModule to setup postgres

---
+++ src/core/database/database.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnvConfigService } from 'src/config/env-config.service';
+import { EnvConfigModule } from '../../config/config.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [EnvConfigModule],
+      inject: [EnvConfigService],
+      useFactory: (envConfigService: EnvConfigService) => {
+        const dbConfig = envConfigService.postgres;
+        const nodeConfig = envConfigService.node;
+
+        return {
+          type: 'postgres',
+          host: dbConfig.host,
+          port: dbConfig.port,
+          username: dbConfig.username,
+          password: dbConfig.password,
+          database: dbConfig.database,
+          autoLoadEntities: true,
+          synchronize: nodeConfig.env !== 'production',
+          logging: nodeConfig.env !== 'production' ? 'all' : ['error'],
+        };
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
