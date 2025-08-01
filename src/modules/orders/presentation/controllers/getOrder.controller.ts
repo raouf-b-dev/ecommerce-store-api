8633c5ef19486facab9d5d1357f4eb19a6c2129a
PRIMERaouf <rbdzmain@gmail.com>
2025-08-01 11:35:47 +0100

    fix imports

--- src/modules/orders/presentation/controllers/getOrder.controller.ts
+++ src/modules/orders/presentation/controllers/getOrder.controller.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { GetOrderUseCase } from '../../application/use-cases/getOrder.usecase';
-import { Order } from '../../domain/entities/order.entity';
+import { Order } from '../../domain/entities/order';
 
 @Injectable()
 export class GetOrderController {
