9d52755f60729e4d01c8fe3149166c8493c4aec2
PRIMERaouf <rbdzmain@gmail.com>
2025-08-02 19:27:37 +0100

    improve return types for GetOrderUseCase and GetOrderController to follow result and error patterns and update code to follow them

--- src/modules/orders/application/use-cases/GetOrder/getOrder.usecase.ts
+++ src/modules/orders/application/use-cases/GetOrder/getOrder.usecase.ts
@@ -2,17 +2,28 @@
 import { Injectable } from '@nestjs/common';
 import { Order } from '../../../domain/entities/order';
 import { OrderRepository } from '../../../domain/repositories/order-repository';
+import { UseCase } from '../../../../../core/application/use-cases/base.usecase';
+import { isFailure, Result } from '../../../../../core/domain/result';
+import { UseCaseError } from '../../../../../core/errors/usecase.error';
+import { ErrorFactory } from '../../../../../core/errors/error.factory';
 
 @Injectable()
-export class GetOrderUseCase {
-  constructor(private readonly orderRepository: OrderRepository) {}
-  async execute(id: number): Promise<Order> {
+export class GetOrderUseCase extends UseCase<number, Order, UseCaseError> {
+  constructor(private readonly orderRepository: OrderRepository) {
+    super();
+  }
+
+  async execute(id: number): Promise<Result<Order, UseCaseError>> {
     try {
-      const order = await this.orderRepository.findById(id);
-      if (!order) throw new Error(`Order with id ${id} not found`);
-      return order;
+      const orderResult = await this.orderRepository.findById(id);
+
+      if (isFailure(orderResult)) {
+        return ErrorFactory.UseCaseError(`Order with id ${id} not found`);
+      }
+
+      return Result.success(orderResult.value);
     } catch (error) {
-      throw error;
+      return ErrorFactory.UseCaseError('Unexpected use case error', error);
     }
   }
 }
--- src/modules/orders/presentation/controllers/GetOrder/get-order.controller.ts
+++ src/modules/orders/presentation/controllers/GetOrder/get-order.controller.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { GetOrderUseCase } from '../../../application/use-cases/GetOrder/getOrder.usecase';
 import { Order } from '../../../domain/entities/order';
+import { ControllerError } from '../../../../../core/errors/controller.error';
+import { isFailure, Result } from '../../../../../core/domain/result';
+import { ErrorFactory } from '../../../../../core/errors/error.factory';
 
 @Injectable()
 export class GetOrderController {
   constructor(private getOrderUseCase: GetOrderUseCase) {}
-  async handle(id: number): Promise<Order> {
+  async handle(id: number): Promise<Result<Order, ControllerError>> {
     try {
-      const order = await this.getOrderUseCase.execute(id);
-      return order;
+      const orderResult = await this.getOrderUseCase.execute(id);
+      if (isFailure(orderResult)) {
+        return ErrorFactory.ControllerError(
+          'Controller failed to get order',
+          orderResult.error,
+        );
+      }
+      return Result.success(orderResult.value);
     } catch (error) {
-      throw error;
+      return ErrorFactory.ControllerError('Unexpected controller error', error);
     }
   }
 }
