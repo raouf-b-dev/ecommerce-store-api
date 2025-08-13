558742670070aeeafef263e550142f2547df5e6e
PRIMERaouf <rbdzmain@gmail.com>
2025-08-13 20:49:06 +0100

    add properties for product entity

--- src/modules/products/domain/entities/product.ts
+++ src/modules/products/domain/entities/product.ts
@@ -3,8 +3,21 @@ import { IProduct } from '../interfaces/IProduct';
 export class Product implements IProduct {
   id: number;
   name: string;
+  description?: string | undefined;
+  price: number;
+  sku?: string | undefined;
+  stockQuantity: number;
+  createdAt: Date;
+  updatedAt: Date;
+
   constructor(productData: IProduct) {
     this.id = productData.id;
     this.name = productData.name;
+    this.description = productData.description;
+    this.price = productData.price;
+    this.sku = productData.sku;
+    this.stockQuantity = productData.stockQuantity;
+    this.createdAt = productData.createdAt;
+    this.updatedAt = productData.updatedAt;
   }
 }
--- src/modules/products/domain/interfaces/IProduct.ts
+++ src/modules/products/domain/interfaces/IProduct.ts
@@ -1,4 +1,10 @@
 export interface IProduct {
   id: number;
   name: string;
+  description?: string;
+  price: number;
+  sku?: string;
+  stockQuantity: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
