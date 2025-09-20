c228525801d26d61f8adf83823997690f056862c
Raouf Bouzerara <rbdzmain@gmail.com>
2025-09-20 22:36:00 +0100

    ES-52: Update CreateOrderDto to include CustomerInfoDto, ShippingAddressDto, and PaymentInfoDto, enhancing order creation structure with validation and optional customer notes.

--- src/modules/orders/presentation/dto/create-order.dto.ts
+++ src/modules/orders/presentation/dto/create-order.dto.ts
@@ -1,12 +1,20 @@
-import { IsString, IsArray, ValidateNested } from 'class-validator';
+// src/modules/orders/presentation/dto/create-order.dto.ts
+import { IsArray, ValidateNested, IsOptional, IsString } from 'class-validator';
 import { Type } from 'class-transformer';
 import { CreateOrderItemDto } from './create-order-item.dto';
-import { ApiProperty } from '@nestjs/swagger';
+import { ShippingAddressDto } from './shipping-address.dto';
+import { CustomerInfoDto } from './customer-info.dto';
+import { PaymentInfoDto } from './payment-info.dto';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateOrderDto {
-  @ApiProperty({ example: 'cust_456', description: 'ID of the customer' })
-  @IsString()
-  customerId: string;
+  @ApiProperty({
+    type: CustomerInfoDto,
+    description: 'Customer information',
+  })
+  @ValidateNested()
+  @Type(() => CustomerInfoDto)
+  customerInfo: CustomerInfoDto;
 
   @ApiProperty({
     type: [CreateOrderItemDto],
@@ -16,4 +24,28 @@ export class CreateOrderDto {
   @ValidateNested({ each: true })
   @Type(() => CreateOrderItemDto)
   items: CreateOrderItemDto[];
+
+  @ApiProperty({
+    type: ShippingAddressDto,
+    description: 'Shipping address details',
+  })
+  @ValidateNested()
+  @Type(() => ShippingAddressDto)
+  shippingAddress: ShippingAddressDto;
+
+  @ApiProperty({
+    type: PaymentInfoDto,
+    description: 'Payment information',
+  })
+  @ValidateNested()
+  @Type(() => PaymentInfoDto)
+  paymentInfo: PaymentInfoDto;
+
+  @ApiPropertyOptional({
+    example: 'Please leave at the front door',
+    description: 'Special delivery instructions',
+  })
+  @IsOptional()
+  @IsString()
+  customerNotes?: string;
 }
