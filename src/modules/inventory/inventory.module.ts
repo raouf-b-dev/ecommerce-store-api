3fc4ebbb733625f666db8bc6437e777cfc0edd6a
Raouf Bouzerara <rbdzmain@gmail.com>
2025-11-01 23:30:50 +0100

    ES-83: Integrate Redis and Postgres repositories in InventoryModule
    - Added TypeOrmModule for InventoryEntity to enable PostgreSQL integration.
    - Implemented dependency injection for PostgresInventoryRepository and RedisInventoryRepository, allowing for seamless inventory management with caching.
    - Configured InventoryRepository to use RedisInventoryRepository as the default, enhancing performance through caching strategies.

--- src/modules/inventory/inventory.module.ts
+++ src/modules/inventory/inventory.module.ts
@@ -7,10 +7,46 @@ import { ReleaseStockController } from './presentation/controllers/release-stock
 import { CheckStockController } from './presentation/controllers/check-stock/check-stock.controller';
 import { ListLowStockController } from './presentation/controllers/list-low-stock/list-low-stock.controller';
 import { BulkCheckStockController } from './presentation/controllers/bulk-check-stock/bulk-check-stock.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  POSTGRES_INVENTORY_REPOSITORY,
+  REDIS_INVENTORY_REPOSITORY,
+} from './inventory.token';
+import { PostgresInventoryRepository } from './infrastructure/repositories/postgres-inventory-repository/postgres-inventory-repository';
+import { RedisInventoryRepository } from './infrastructure/repositories/redis-inventory-repository/redis-inventory-repository';
+import { CacheService } from '../../core/infrastructure/redis/cache/cache.service';
+import { InventoryRepository } from './domain/repositories/inventory.repository';
+import { RedisModule } from '../../core/infrastructure/redis/redis.module';
+import { InventoryEntity } from './infrastructure/orm/inventory.schema';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([InventoryEntity]), RedisModule],
   controllers: [InventoryController],
   providers: [
+    //Postgres Repo
+    {
+      provide: POSTGRES_INVENTORY_REPOSITORY,
+      useClass: PostgresInventoryRepository,
+    },
+
+    // Redis Repo (decorator around Postgres)
+    {
+      provide: REDIS_INVENTORY_REPOSITORY,
+      useFactory: (
+        cacheService: CacheService,
+        postgresRepo: PostgresInventoryRepository,
+      ) => {
+        return new RedisInventoryRepository(cacheService, postgresRepo);
+      },
+      inject: [CacheService, POSTGRES_INVENTORY_REPOSITORY],
+    },
+
+    // Default Repository Binding
+    {
+      provide: InventoryRepository,
+      useExisting: REDIS_INVENTORY_REPOSITORY,
+    },
+
     //Controllers
     GetInventoryController,
     AdjustStockController,
