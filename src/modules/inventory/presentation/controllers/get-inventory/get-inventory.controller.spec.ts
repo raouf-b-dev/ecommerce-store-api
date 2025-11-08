cc92ccc63e40198bc56f21d1b716074cf9e0b721
Raouf Bouzerara <rbdzmain@gmail.com>
2025-11-08 11:43:46 +0100

    Enhance GetInventoryController with unit tests and error handling
    - Updated GetInventoryController to utilize GetInventoryUseCase for inventory retrieval by product ID.
    - Implemented error handling for both successful and failed inventory retrieval scenarios.
    - Added unit tests to validate the controller's behavior, including success cases and handling of unexpected exceptions.

--- src/modules/inventory/presentation/controllers/get-inventory/get-inventory.controller.spec.ts
+++ src/modules/inventory/presentation/controllers/get-inventory/get-inventory.controller.spec.ts
@@ -1,10 +1,24 @@
+import { GetInventoryUseCase } from '../../../application/get-inventory/get-inventory.usecase';
 import { GetInventoryController } from './get-inventory.controller';
+import { Result } from '../../../../../core/domain/result';
+import { IInventory } from '../../../domain/interfaces/inventory.interface';
+import { InventoryTestFactory } from '../../../testing/factories/inventory.test.factory';
+import { ResultAssertionHelper } from '../../../../../testing/helpers/result-assertion.helper';
+import { ControllerError } from '../../../../../core/errors/controller.error';
+import { RepositoryError } from '../../../../../core/errors/repository.error';
 
 describe('GetInventoryController', () => {
+  let usecase: jest.Mocked<GetInventoryUseCase>;
   let controller: GetInventoryController;
 
-  beforeEach(async () => {
-    controller = new GetInventoryController();
+  const inventoryFactory = InventoryTestFactory;
+
+  beforeEach(() => {
+    usecase = {
+      execute: jest.fn(),
+    } as any;
+
+    controller = new GetInventoryController(usecase);
   });
 
   afterEach(() => {
@@ -13,5 +27,57 @@ describe('GetInventoryController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return the inventory on success', async () => {
+    // Arrange
+    const productId = 'prod-123';
+    const mockInventory: IInventory =
+      inventoryFactory.createInventoryForProduct(productId);
+
+    usecase.execute.mockResolvedValue(Result.success(mockInventory));
+
+    // Act
+    const result = await controller.handle(productId);
+
+    // Assert
+    expect(usecase.execute).toHaveBeenCalledWith(productId);
+    ResultAssertionHelper.assertResultSuccess(result);
+    expect(result.value).toEqual(mockInventory);
+  });
+
+  it('should return a failure result if the use case fails', async () => {
+    // Arrange
+    const productId = 'prod-404';
+    const mockError = new RepositoryError('Inventory not found');
+
+    usecase.execute.mockResolvedValue(Result.failure(mockError));
+
+    // Act
+    const result = await controller.handle(productId);
+
+    // Assert
+    expect(usecase.execute).toHaveBeenCalledWith(productId);
+    ResultAssertionHelper.assertResultFailureWithError(result, mockError);
+  });
+
+  it('should return a ControllerError on unexpected exceptions', async () => {
+    // Arrange
+    const productId = 'prod-500';
+    const mockError = new Error('Database connection failed');
+
+    usecase.execute.mockRejectedValue(mockError);
+
+    // Act
+    const result = await controller.handle(productId);
+
+    // Assert
+    expect(usecase.execute).toHaveBeenCalledWith(productId);
+    ResultAssertionHelper.assertResultFailure(
+      result,
+      'Unexpected Controller Error',
+      ControllerError,
+      mockError,
+    );
   });
 });
--- src/modules/inventory/presentation/controllers/get-inventory/get-inventory.controller.ts
+++ src/modules/inventory/presentation/controllers/get-inventory/get-inventory.controller.ts
@@ -2,13 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { Result } from '../../../../../core/domain/result';
 import { ControllerError } from '../../../../../core/errors/controller.error';
 import { ErrorFactory } from '../../../../../core/errors/error.factory';
+import { GetInventoryUseCase } from '../../../application/get-inventory/get-inventory.usecase';
+import { IInventory } from '../../../domain/interfaces/inventory.interface';
 
 @Injectable()
 export class GetInventoryController {
-  constructor() {}
-  async handle(productId: string): Promise<Result<void, ControllerError>> {
+  constructor(private getInventoryUseCase: GetInventoryUseCase) {}
+  async handle(
+    productId: string,
+  ): Promise<Result<IInventory, ControllerError>> {
     try {
-      return Result.success(undefined);
+      const result = await this.getInventoryUseCase.execute(productId);
+      if (result.isFailure) return result;
+
+      return Result.success(result.value);
     } catch (error) {
       return ErrorFactory.ControllerError('Unexpected Controller Error', error);
     }
