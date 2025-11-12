a3e900969a129f2d29716d9ba3ce3e81fb5cb7ec
Raouf Bouzerara <rbdzmain@gmail.com>
2025-11-12 07:16:56 +0100

    ES-84: Enhance Quantity value object with validation and error handling
    - Introduced validation in the Quantity constructor to ensure the value is an integer and non-negative, throwing DomainError for invalid inputs.
    - Refactored the subtract method to return a Result type, providing better error handling for negative results instead of throwing an error directly.
    - Improved overall robustness of the Quantity class by integrating error handling mechanisms.

--- src/shared/domain/value-objects/quantity.ts
+++ src/shared/domain/value-objects/quantity.ts
@@ -1,15 +1,27 @@
 // src/shared/domain/value-objects/quantity.ts
+import { Result } from '../../../core/domain/result';
+import { DomainError } from '../../../core/errors/domain.error';
+import { ErrorFactory } from '../../../core/errors/error.factory';
+
 export class Quantity {
   private readonly _value: number;
 
   constructor(value: number) {
+    const validationResult = this.validateProps(value);
+    if (validationResult.isFailure) throw validationResult.error;
+
+    this._value = value;
+  }
+
+  private validateProps(value: number): Result<void, DomainError> {
     if (!Number.isInteger(value)) {
-      throw new Error('Quantity must be an integer');
+      throw new DomainError('Quantity must be an integer');
     }
     if (value < 0) {
-      throw new Error('Quantity cannot be negative');
+      throw new DomainError('Quantity cannot be negative');
     }
-    this._value = value;
+
+    return Result.success(undefined);
   }
 
   get value(): number {
@@ -28,12 +40,14 @@ export class Quantity {
     return new Quantity(this._value + other._value);
   }
 
-  subtract(other: Quantity): Quantity {
+  subtract(other: Quantity): Result<Quantity, DomainError> {
     const result = this._value - other._value;
     if (result < 0) {
-      throw new Error('Cannot subtract: result would be negative');
+      return ErrorFactory.DomainError(
+        'Cannot subtract: result would be negative',
+      );
     }
-    return new Quantity(result);
+    return Result.success(new Quantity(result));
   }
 
   isGreaterThan(other: Quantity): boolean {
