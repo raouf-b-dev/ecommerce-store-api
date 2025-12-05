b44da60208ee791f1c0e7fef9774af7979d236ef
Raouf Bouzerara <rbdzmain@gmail.com>
2025-12-05 18:10:03 +0100

    ES-96: create user testing mocks, factories and builders

---
+++ src/modules/auth/testing/mocks/bcrypt-service.mock.ts
@@ -0,0 +1,12 @@
+import { BcryptService } from '../../infrastructure/services/bcrypt.service';
+
+export class MockBcryptService extends BcryptService {
+  hash = jest.fn<Promise<string>, [string]>();
+  compare = jest.fn<Promise<boolean>, [string, string]>();
+
+  constructor() {
+    super();
+    this.hash.mockResolvedValue('hashed_password');
+    this.compare.mockResolvedValue(true);
+  }
+}
