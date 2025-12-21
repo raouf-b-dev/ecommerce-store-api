37c443f181b21f46266344fcf0a61ac433d12cba
Raouf Bouzerara <rbdzmain@gmail.com>
2025-12-21 22:43:06 +0100

    ES-71: Add IdempotencyModule to manage idempotency in order processing. This module integrates IdempotencyService and IdempotencyStore, utilizing Redis for key management and enhancing the overall robustness of the checkout workflow. Update infrastructure module to include IdempotencyModule in imports and exports.

--- src/core/infrastructure/infrastructure.module.ts
+++ src/core/infrastructure/infrastructure.module.ts
@@ -4,16 +4,24 @@ import { RedisModule } from './redis/redis.module';
 import { QueueModule } from './queue/queue.module';
 import { JobsModule } from './jobs/jobs.module';
 import { IdGeneratorService } from './orm/id-generator.service';
+import { IdempotencyModule } from './idempotency/idempotency.module';
 
 @Global()
 @Module({
-  imports: [DatabaseModule, RedisModule, QueueModule, JobsModule],
+  imports: [
+    DatabaseModule,
+    RedisModule,
+    QueueModule,
+    JobsModule,
+    IdempotencyModule,
+  ],
   providers: [IdGeneratorService],
   exports: [
     DatabaseModule,
     RedisModule,
     QueueModule,
     JobsModule,
+    IdempotencyModule,
     IdGeneratorService,
   ],
 })
