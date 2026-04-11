f27d2f48b65ba5bf40bf201e8738afb64346fb2e
Raouf Bouzerara <rbdzmain@gmail.com>
2026-04-11 21:01:46 +0100

    ES-107: Enhance CreateOrderItemDto with productName and unitPrice properties: Add validation and API documentation for new fields to improve order item structure and clarity in order processing.

--- src/modules/orders/primary-adapters/dto/create-order-item.dto.ts
+++ src/modules/orders/primary-adapters/dto/create-order-item.dto.ts
@@ -1,10 +1,18 @@
-import { IsNumber, Min } from 'class-validator';
+import { IsNumber, IsString, Min } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateOrderItemDto {
   @ApiProperty({ example: 123, description: 'ID of the product' })
   @IsNumber()
   productId: number;
+
+  @ApiProperty({ example: 'Product Name', description: 'Name of the product' })
+  @IsString()
+  productName: string;
+
+  @ApiProperty({ example: 29.99, description: 'Unit price of the product' })
+  @IsNumber()
+  unitPrice: number;
 
   @ApiProperty({ example: 2, description: 'Quantity ordered' })
   @IsNumber()
