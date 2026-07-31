e68b980d16ca3499a7139bd84cc5622ca4dd8cfc
Raouf Bouzerara <rbdzmain@gmail.com>
2026-07-31 19:04:47 +0100

    ES-139: Refactor PaymentMethod Enum and Methods
    - Removed unused payment method types: CREDIT_CARD, DEBIT_CARD, CASH_ON_DELIVERY, BANK_TRANSFER, DIGITAL_WALLET, and PAYPAL from the PaymentMethodType enum.
    - Simplified the requiresGateway method to always return true, reflecting a change in payment processing logic.
    - Deleted isCardPayment and isCOD methods to streamline the PaymentMethod class and enhance code clarity.

--- src/shared-kernel/domain/value-objects/payment-method.ts
+++ src/shared-kernel/domain/value-objects/payment-method.ts
@@ -5,13 +5,7 @@
 import { DomainError } from '../exceptions/domain.error';
 
 export enum PaymentMethodType {
-  CREDIT_CARD = 'CREDIT_CARD',
-  DEBIT_CARD = 'DEBIT_CARD',
-  CASH_ON_DELIVERY = 'CASH_ON_DELIVERY',
-  BANK_TRANSFER = 'BANK_TRANSFER',
-  DIGITAL_WALLET = 'DIGITAL_WALLET',
   STRIPE = 'STRIPE',
-  PAYPAL = 'PAYPAL',
 }
 
 export class PaymentMethod {
@@ -28,25 +22,8 @@ export class PaymentMethod {
     return this._type;
   }
 
-  isCardPayment(): boolean {
-    return [
-      PaymentMethodType.CREDIT_CARD,
-      PaymentMethodType.DEBIT_CARD,
-    ].includes(this._type);
-  }
-
-  isCOD(): boolean {
-    return this._type === PaymentMethodType.CASH_ON_DELIVERY;
-  }
-
   requiresGateway(): boolean {
-    return [
-      PaymentMethodType.STRIPE,
-      PaymentMethodType.PAYPAL,
-      PaymentMethodType.CREDIT_CARD,
-      PaymentMethodType.DEBIT_CARD,
-      PaymentMethodType.BANK_TRANSFER,
-    ].includes(this._type);
+    return true;
   }
 
   equals(other: PaymentMethod): boolean {
