42b1846aa1b3da3ea52cd944140da2ab23bfe5f7
abderaouf <rbdzmain@gmail.com>
2026-09-12 12:42:47 +0100

    Refactor Cart Presentation DTO to Enhance Structure and Clarity
    - Updated CartItemPresentationDTO to replace itemTotal with subtotal and added currency field for better financial representation.
    - Modified CartPresentationDTO to rename totalQuantity to itemCount, grandTotal to totalAmount, and included currency and createdAt fields for improved data handling.

--- src/modules/carts/core/application/queries/results/cart-presentation.result.ts
+++ src/modules/carts/core/application/queries/results/cart-presentation.result.ts
@@ -3,8 +3,9 @@ export interface CartItemPresentationDTO {
   productId: number;
   productName: string;
   price: number;
+  currency: string;
   quantity: number;
-  itemTotal: number;
+  subtotal: number;
   imageUrl: string | null;
 }
 
@@ -12,7 +13,9 @@ export interface CartPresentationDTO {
   id: number;
   userId: number;
   items: CartItemPresentationDTO[];
-  totalQuantity: number;
-  grandTotal: number;
+  itemCount: number;
+  totalAmount: number;
+  currency: string | null;
+  createdAt: string;
   updatedAt: string;
 }
